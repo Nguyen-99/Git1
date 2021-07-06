9b03a5005c015aa64295e28434ac1969ecf62a84
unknown <thanmeo4@gmail.com>
2021-07-06 10:22:59 +0700

    git3

--- src/com/nguyenz/Main.java
+++ src/com/nguyenz/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello Git 1");
 	    System.out.println("Hello Git 2");
+        System.out.println("Hello Git 3");
     }
 }
