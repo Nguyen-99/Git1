dbcab3e1d25c99b8ba93c057d3635495f5330056
unknown <thanmeo4@gmail.com>
2021-07-06 09:43:59 +0700

    add pápeople class

---
+++ src/com/nguyenz/People.java
@@ -0,0 +1,34 @@
+package com.nguyenz;
+
+public class People {
+    private int id;
+    private String name;
+    private byte age;
+
+    public People() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+}
