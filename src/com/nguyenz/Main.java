5ff0bee8a5d51c9d85d96988d7c7cb208f903083
unknown <thanmeo4@gmail.com>
2021-07-06 10:31:55 +0700

    /

--- src/com/nguyenz/Main.java
+++ src/com/nguyenz/Main.java
@@ -3,7 +3,6 @@ package com.nguyenz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello Git 1");
 	    System.out.println("Hello Git 2");
         System.out.println("Hello Git 3");
