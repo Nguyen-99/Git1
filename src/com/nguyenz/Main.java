385acf2ea3eb5a0d995d9a93b779ead9d71c9c4b
Nguyen-99 <67599613+Nguyen-99@users.noreply.github.com>
2021-07-07 11:01:28 +0700

    Update Main.java

--- src/com/nguyenz/Main.java
+++ src/com/nguyenz/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git 1");
-        People p=new Developer(1,"Nguyen", (byte) 22,"java_backend","luvina");
+        People p=new Developer(1,"NguyenKhoiNguyen", (byte) 22,"java_fullstack_developer","luvina");
         System.out.println(p.getName());
         System.out.println(p);
         p.work();
